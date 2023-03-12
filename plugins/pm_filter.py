ab8f897ea2a504d427f94e9d43a3096c6b6ede01
skalight <119775283+skalight@users.noreply.github.com>
2023-03-12 14:51:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -30,22 +30,11 @@ BUTTONS = {}
 SPELL_CHECK = {}
 FILTER_MODE = {}
 
-@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
+@@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
-    if message.chat.id != SUPPORT_CHAT_ID:
-        await global_filters(client, message)
-    manual = await manual_filters(client, message)
-    if manual == False:
-        settings = await get_settings(message.chat.id)
-        try:
-            if settings['auto_ffilter']:
-                await auto_filter(client, message)
-        except KeyError:
-            grpid = await active_connection(str(message.from_user.id))
-            await save_group_settings(grpid, 'auto_ffilter', True)
-            settings = await get_settings(message.chat.id)
-            if settings['auto_ffilter']:
-                await auto_filter(client, message) 
+    k = await manual_filters(client, message)
+    if k == False:
+        await auto_filter(client, message)  
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
